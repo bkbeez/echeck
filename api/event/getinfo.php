e129e5cb445e2a04a5a0692e5c8e4ee99bcb7212
BKBEEZ <bkbeez@gmail.com>
2025-12-12 20:53:09 +0700

    Add api/event

---
+++ api/event/getinfo.php
@@ -0,0 +1,35 @@
+<?php include('../../app/autoload.php'); ?>
+<?php
+    if( !isset($_SERVER['PHP_AUTH_USER'])||!isset($_SERVER['PHP_AUTH_USER']) ){
+        header('HTTP/1.1 401 Authorization Required');
+        header('WWW-Authenticate: Basic realm="Access denied"');
+        header("Content-Type: application/json; charset=UTF-8");
+        http_response_code(401);
+        echo json_encode(['status' => 'error', 'message' => 'Login Required']);
+        exit();
+    }else{
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $json_payload = file_get_contents('php://input');
+            $input = json_decode($json_payload, true);
+            if(json_last_error() === JSON_ERROR_NONE){
+                if( isset($input['events_id'])&&$input['events_id'] ){
+                    $info = DB::one("SELECT * FROM events WHERE events_id=:events_id LIMIT 1;", array('events_id'=>$input['events_id']));
+                    http_response_code(200);
+                    echo json_encode(['status' => 'success', 'info'=>$info]);
+                    exit();
+                }else{
+                    http_response_code(400);
+                    echo json_encode(['status' => 'error', 'message' => 'Not found events_id']);
+                    exit();
+                }
+            }else{
+                http_response_code(400);
+                echo json_encode(['status' => 'error', 'message' => 'Invalid JSON payload']);
+                exit();
+            }
+        }
+        http_response_code(405);
+        header('Allow: POST');
+        echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+    }
+?>
