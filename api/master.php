dd56a5a955180568809bf43a2c6408cfec3fdd29
Panuwich Thammawong <fighterkab123@gmail.com>
2025-12-19 10:40:05 +0700

    update

---
+++ api/master.php
@@ -0,0 +1,10 @@
+<?php
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.edu.cmu.ac.th/v1/student/master/2568');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, 'checkin:Check$in1215');
+    $result = curl_exec($ch);
+    curl_close($ch);  
+    echo $result;
+?>
