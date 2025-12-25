1d4105d254224ac56c9220d9782ebdafbb0a65dd
Somboon Muensrithi <bkbeez@gmail.com>
2025-12-25 15:51:33 +0700

    Add datepicker

--- app/assets/header.php
+++ app/assets/header.php
@@ -22,11 +22,15 @@
         <link rel="stylesheet" href="<?=THEME_CSS?>/index.css">
         <link rel="stylesheet" href="<?=THEME_CSS?>/table.css">
         <link rel="stylesheet" href="<?=THEME_JS?>/sweetalert/sweetalert2.min.css" />
+        <link rel="stylesheet" href="<?=THEME_JS?>/datepicker/thai/datepicker.css" />
         <script type="text/javascript" src="<?=THEME_JS?>/jquery-2.1.1.js"></script>
         <script type="text/javascript" src="<?=THEME_JS?>/jquery.form.min.js"></script>
         <script type="text/javascript" src="<?=THEME_JS?>/sweetalert/sweetalert2.min.js"></script>
         <script type="text/javascript" src="<?=THEME_JS?>/qrcode/html5-qrcode.min.js?<?=time()?>"></script>
         <script type="text/javascript" src="<?=THEME_JS?>/qrcode/jquery.qrcode-0.12.0.min.js"></script>
+        <script type="text/javascript" src="<?=THEME_JS?>/datepicker/thai/bootstrap-datepicker.js"></script>
+        <script type="text/javascript" src="<?=THEME_JS?>/datepicker/thai/bootstrap-datepicker-thai.js"></script>
+        <script type="text/javascript" src="<?=THEME_JS?>/datepicker/thai/locales/bootstrap-datepicker.th.js"></script>
         <script type="text/javascript" src="<?=THEME_JS?>/index.js?<?=time()?>"></script>
     </head>
     <body>
