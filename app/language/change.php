58b73e38b32c300e50a0c7bab15da3aa35a6e8c3
BKBEEZ <bkbeez@gmail.com>
2025-12-28 15:00:30 +0700

    Update change.php

--- app/language/change.php
+++ app/language/change.php
@@ -1,5 +1,5 @@
 <?php include($_SERVER["DOCUMENT_ROOT"].'/app/autoload.php'); ?>
 <?php
-    $_SESSION['NICE_LANGUAGE'] = ( (isset($_POST['lang'])&&$_POST['lang']) ? $_POST['lang'] : 'th' );
+    $_SESSION['SITE_LANGUAGE'] = ( (isset($_POST['lang'])&&$_POST['lang']) ? $_POST['lang'] : 'th' );
     Status::success( ( (App::lang()=='en') ? 'Language was changed' : 'เปลี่ยนภาษาเรียบร้อยแล้ว' ) );
 ?>
