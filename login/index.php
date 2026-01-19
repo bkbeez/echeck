6e0ca84e7216bd7c43202b1fb8489fbdb794d81c
Somboon Muensrithi <bkbeez@gmail.com>
2026-01-19 10:22:16 +0700

    Update index.php

--- login/index.php
+++ login/index.php
@@ -37,7 +37,7 @@
         <link rel="apple-touch-icon-precomposed" href="<?=APP_PATH?>/favicon.png" />
         <link rel="stylesheet" href="<?=THEME_CSS?>/plugins.css">
         <link rel="stylesheet" href="<?=THEME_CSS?>/style.css">
-        <link rel="stylesheet" href="<?=THEME_CSS?>/colors/grape.css">
+        <link rel="stylesheet" href="<?=THEME_CSS?>/grape.css">
         <link rel="stylesheet" href="<?=THEME_JS?>/sweetalert/sweetalert2.min.css" />
         <link rel="stylesheet" href="<?=THEME_CSS?>/index.css?<?=time()?>" />
         <script type="text/javascript" src="<?=THEME_JS?>/jquery-2.1.1.js"></script>
